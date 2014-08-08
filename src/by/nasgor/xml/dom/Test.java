d1d4cf3700d81f51eafffe83846cf3737346b9db
nasgor <https://bitbucket.org/nasgor/ribbon>
2014-08-08 15:57:05 +0300

    add observer pattern classes

---
+++ src/by/nasgor/xml/dom/Test.java
@@ -0,0 +1,10 @@
+package by.nasgor.xml.dom;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nasgor
+ * Date: 8/8/14
+ * Time: 3:56 PM
+ */
+public class Test {
+}
---
+++ src/by/nasgor/xml/jdom/Test.java
@@ -0,0 +1,10 @@
+package by.nasgor.xml.jdom;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nasgor
+ * Date: 8/8/14
+ * Time: 3:56 PM
+ */
+public class Test {
+}
