7db9e6e28df4e0655dac10e0c96b26edac32c25f
nasgor <https://bitbucket.org/nasgor/ribbon>
2014-08-13 19:48:01 +0300

    add some code

--- src/by/nasgor/logging/TestLogger.java
+++ src/by/nasgor/logging/TestLogger.java
@@ -1,10 +1,30 @@
 package by.nasgor.logging;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.logging.Logger;
+
 /**
  * Created with IntelliJ IDEA.
  * User: nasgor
  * Date: 8/6/14
  * Time: 1:03 PM
  */
-public class TestLogger {
+public class TestLogger extends Exception{
+    private static Logger logger = Logger.getLogger("LoggingException");
+
+
+    public TestLogger(){
+        StringWriter trace = new StringWriter();
+        printStackTrace(new PrintWriter(trace));
+        logger.severe(trace.toString());
+    }
+
+    public static void main(String[] args){
+        try{
+            throw new TestLogger();
+        }catch (TestLogger ex){
+            System.err.println("перехвачено");
+        }
+    }
 }
---
+++ src/by/nasgor/multithreading/Test.java
@@ -0,0 +1,18 @@
+package by.nasgor.multithreading;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nasgor
+ * Date: 8/13/14
+ * Time: 1:25 PM
+ */
+public class Test {
+    public static void main(String[] args){
+        Runnable r1 = new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        };
+    }
+}
