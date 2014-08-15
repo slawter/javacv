7fec19cbe79fec01b8c96bffcced15fada7b4d4a
nasgor <https://bitbucket.org/nasgor/ribbon>
2014-08-15 12:16:26 +0300

    add some code

---
+++ src/by/nasgor/multithreading/Philosopher.java
@@ -0,0 +1,24 @@
+package by.nasgor.multithreading;
+
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nasgor
+ * Date: 8/15/14
+ * Time: 11:59 AM
+ */
+public class Philosopher extends JDialog {
+    public static void main(String[] args){
+
+    }
+
+    private class Phil{
+
+    }
+
+
+    private class Fork{
+
+    }
+}
